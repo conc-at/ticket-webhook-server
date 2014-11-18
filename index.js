fddc34a2bf452a4965a798888d90849554d1d3bd
Christoph Witzko <github@christophwitzko.com>
2014-11-18 14:14:30 +0100

    feat: added tito

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 'use strict'
 
 var app = require('express')()
+var bodyParser = require('body-parser')
 var server = require('http').Server(app)
+
+app.disable('x-powered-by')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/tito/:key', function(req, res){
+  var whname = req.headers['x-webhook-name']
+  if(req.params.key  && req.params.key === process.env.TITO_ACCESS_KEY && whname){
+    var created = whname === 'ticket.created'
+    if(created || whname === 'ticket.updated'){
+      console.log(req.body)
+      res.send('ok')
+    }
+    else res.send('error')
+  }
+  else res.send('error')
+})
 
 server.listen(process.env.PORT, function() {
   console.log('listening on port %d', process.env.PORT)
