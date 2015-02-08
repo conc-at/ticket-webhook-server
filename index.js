d861e66b7b70315fbc86c5bf66a48ef619913900
Christoph Witzko <github@christophwitzko.com>
2015-02-09 00:06:26 +0100

    refactor: using standard

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ var app = require('express')()
 var bodyParser = require('body-parser')
 var server = require('http').Server(app)
 
-if(!process.env.SLACK_WEBHOOK_URL || !process.env.TITO_ACCESS_KEY) return console.log('no SLACK_WEBHOOK_URL or TITO_ACCESS_KEY set')
+if (!process.env.SLACK_WEBHOOK_URL || !process.env.TITO_ACCESS_KEY) {
+  console.log('no SLACK_WEBHOOK_URL or TITO_ACCESS_KEY set')
+  process.exit(1)
+}
 
 var slack = require('slack-notify')(process.env.SLACK_WEBHOOK_URL)
 
-slack.onError = function(err){
+slack.onError = function (err) {
   console.log('slack error:', err)
 }
 
@@ -22,27 +25,25 @@ var ticketSlack = slack.extend({
 app.disable('x-powered-by')
 app.use(bodyParser.json())
 
-app.post('/tito/:key', function(req, res){
+app.post('/tito/:key', function (req, res) {
   console.log('new tito request')
   var whname = req.headers['x-webhook-name']
-  if(req.params.key  && req.params.key === process.env.TITO_ACCESS_KEY && whname){
-    if(req.body.state_name === 'complete'){
+  if (req.params.key && req.params.key === process.env.TITO_ACCESS_KEY && whname) {
+    if (req.body.state_name === 'complete') {
       var msg = '"' + req.body.name + '" bought/updated a new ticket. (' + req.body.release + ')'
       console.log('send to slack:', msg)
       ticketSlack(msg)
-    }
-    else {
+    } else {
       console.log('skipped webhook:', whname, '-', req.body.state_name)
     }
     res.send('ok')
-  }
-  else res.send('error')
+  } else res.send('error')
 })
 
-app.get('/ping', function(req, res){
+app.get('/ping', function (req, res) {
   res.send('ok')
 })
 
-server.listen(process.env.PORT, function() {
+server.listen(process.env.PORT, function () {
   console.log('listening on port %d', process.env.PORT)
 })
